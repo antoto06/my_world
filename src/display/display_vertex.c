20c023b1162263d56dae6cf6f64b12aaba7fb249
GRAS Lucas <lucas.gras@hotmail.fr>
2018-01-30 19:52:53 +0100

    Display isometric shape is working, creation of relief is working as well

--- src/display/display_vertex.c
+++ src/display/display_vertex.c
@@ -12,12 +12,18 @@ int display_vertex(sfRenderWindow *window, sfVector2f **map2d)
 	int i = 0;
 	int j = 0;
 	sfVertexArray *tmp;
+	sfVertexArray *tmp_inv;
 
 	while (i < MAP_X) {
 		while (j < MAP_Y) {
 			if (j != 0) {
 				tmp = create_line(&map2d[i][j], &map2d[i][j - 1]);
 				sfRenderWindow_drawVertexArray(window, tmp, sfFalse);
+			}
+			if (i != 0) {
+				tmp_inv = create_line(&map2d[i][j], &map2d[i - 1][j]);
+				sfRenderWindow_drawVertexArray(window, tmp_inv, sfFalse);
+
 			}
 			j++;
 		}
