f4561a13c301a2d4e46e2ae17f5d864a50c8e5ac
tom rouvier <tom.rouvier@epitech.eu>
2018-02-04 16:23:35 +0100

    updateafter merge

--- src/parser/reading.c
+++ src/parser/reading.c
@@ -12,13 +12,7 @@ input_map_t my_rd(char *str)
 	FILE *fp = fopen(str, "r");
 	char s[1024];
 
-<<<<<<< HEAD
 	while (fread(s, 1, 1, fp));
 	return (my_str_to_int_array(s, fp));
-=======
-	while (s)
-		fread(s, 1, 1, fp);
-	return (my_str_to_int_array(s));
->>>>>>> cef05de8e4f779453e1bd664e800593925324eaf
 }
 
