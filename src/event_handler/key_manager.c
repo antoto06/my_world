25f72a4e35987f7bc8e62d1dac5a554dfb9b2cb1
GRAS Lucas <lucas.gras@hotmail.fr>
2018-02-27 18:48:24 +0100

    [+] Starting keyboard shortcuts

--- src/event_handler/key_manager.c
+++ src/event_handler/key_manager.c
@@ -19,6 +19,19 @@ void key_translate(map_node_t **map2d, sfKeyEvent key)
 		button_translate(map2d, 1, 0);
 }
 
+void shortcut_tools(sfKeyEvent key, window_t *window)
+{
+	if (window->window_ui.ui.tools_state.elevate == sfFalse
+	&& key.code == sfKeyE) {
+		/* change_button_texture(buttons_tmp[0], BUTTON_GREEN_BURN); */
+		/* window->window_ui.tools_state.elevate = sfTrue; */
+	} else if (window->window_ui.ui.tools_state.elevate == sfTrue
+	&& key.code == sfKeyD) {
+		/* change_button_texture(buttons_tmp[0], BUTTON_GREEN_BURN); */
+		/* window->window_ui.tools_state.elevate = sfTrue; */
+	}
+}
+
 void key_manager(window_t *window, sfKeyEvent key ,map_node_t **map2d)
 {
 	key_translate(map2d, key);
