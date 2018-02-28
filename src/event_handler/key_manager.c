bf9fe99e67b52a66c2b9ec98754b3ae5f1e62661
GRAS Lucas <lucas.gras@hotmail.fr>
2018-02-28 18:24:39 +0100

    [+] Shortcuts for elevate and dig buttons

--- src/event_handler/key_manager.c
+++ src/event_handler/key_manager.c
@@ -21,19 +21,32 @@ void key_translate(map_node_t **map2d, sfKeyEvent key)
 
 void shortcut_tools(sfKeyEvent key, window_t *window)
 {
-	if (window->window_ui.ui.tools_state.elevate == sfFalse
+	button_t button_up = window->window_ui.button_tools[0];
+	button_t button_down = window->window_ui.button_tools[1];
+
+	if (window->window_ui.tools_state.elevate == sfFalse
 	&& key.code == sfKeyE) {
-		/* change_button_texture(buttons_tmp[0], BUTTON_GREEN_BURN); */
-		/* window->window_ui.tools_state.elevate = sfTrue; */
-	} else if (window->window_ui.ui.tools_state.elevate == sfTrue
+		change_button_texture(button_up, BUTTON_GREEN_BURN);
+		window->window_ui.tools_state.elevate = sfTrue;
+	} else if (window->window_ui.tools_state.elevate == sfTrue
+	&& key.code == sfKeyE) {
+		change_button_texture(button_up, BUTTON_GREEN);
+		window->window_ui.tools_state.elevate = sfFalse;
+	}
+	if (window->window_ui.tools_state.dig == sfFalse
 	&& key.code == sfKeyD) {
-		/* change_button_texture(buttons_tmp[0], BUTTON_GREEN_BURN); */
-		/* window->window_ui.tools_state.elevate = sfTrue; */
+		change_button_texture(button_down, BUTTON_GREEN_BURN);
+		window->window_ui.tools_state.dig = sfTrue;
+	} else if (window->window_ui.tools_state.dig == sfTrue
+	&& key.code == sfKeyD) {
+		change_button_texture(button_down, BUTTON_GREEN);
+		window->window_ui.tools_state.dig = sfFalse;
 	}
 }
 
 void key_manager(window_t *window, sfKeyEvent key ,map_node_t **map2d)
 {
 	key_translate(map2d, key);
+	shortcut_tools(key, window);
 	(void)window;
 }
